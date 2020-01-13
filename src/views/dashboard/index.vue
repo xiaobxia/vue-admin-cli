fc0a67c294f3015d75edb06c0d18df6c65944b50
xiaobxia <673806687@qq.com>
2020-01-13 10:14:01 +0800

    xg

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -4,6 +4,24 @@
       title="主页"
       type="success"/>
     <el-button type="primary" @click="exportFile">主要按钮</el-button>
+    <el-tree
+      ref="tree"
+      :data="data"
+      :props="defaultProps"
+      :check-strictly="true"
+      show-checkbox
+      default-expand-all
+      node-key="id"
+      highlight-current
+      @check="treeCheck"/>
+
+    <div class="buttons">
+      <el-button @click="getCheckedNodes">通过 node 获取</el-button>
+      <el-button @click="getCheckedKeys">通过 key 获取</el-button>
+      <el-button @click="setCheckedNodes">通过 node 设置</el-button>
+      <el-button @click="setCheckedKeys">通过 key 设置</el-button>
+      <el-button @click="resetChecked">清空</el-button>
+    </div>
   </div>
 </template>
 
@@ -14,6 +32,45 @@ export default {
   components: { },
   data() {
     return {
+      data: [{
+        id: 1,
+        label: '一级 1',
+        children: [{
+          id: 4,
+          label: '二级 1-1',
+          children: [{
+            id: 9,
+            label: '三级 1-1-1'
+          }, {
+            id: 10,
+            label: '三级 1-1-2'
+          }]
+        }]
+      }, {
+        id: 2,
+        label: '一级 2',
+        children: [{
+          id: 5,
+          label: '二级 2-1'
+        }, {
+          id: 6,
+          label: '二级 2-2'
+        }]
+      }, {
+        id: 3,
+        label: '一级 3',
+        children: [{
+          id: 7,
+          label: '二级 3-1'
+        }, {
+          id: 8,
+          label: '二级 3-2'
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
     }
   },
   computed: {
@@ -25,6 +82,41 @@ export default {
       this.$http.get('download/exportXlsx', {}, { responseType: 'arraybuffer' }).then((res) => {
         this.downloadExcel(res, '用户列表', 'xlsx')
       })
+    },
+    treeCheck(a, b) {
+      if (a.children && a.children.length > 0) {
+        const rowCheckedKeys = b.checkedKeys
+        if (rowCheckedKeys.indexOf(a.id) !== 1) {
+          // 选中状态
+          a.children.forEach((item) => {
+          })
+        } else {
+          // 取消状态
+        }
+      }
+      console.log(a)
+      console.log(b)
+    },
+    getCheckedNodes() {
+      console.log(this.$refs.tree.getCheckedNodes())
+    },
+    getCheckedKeys() {
+      console.log(this.$refs.tree.getCheckedKeys())
+    },
+    setCheckedNodes() {
+      this.$refs.tree.setCheckedNodes([{
+        id: 5,
+        label: '二级 2-1'
+      }, {
+        id: 9,
+        label: '三级 1-1-1'
+      }])
+    },
+    setCheckedKeys() {
+      this.$refs.tree.setCheckedKeys([3])
+    },
+    resetChecked() {
+      this.$refs.tree.setCheckedKeys([])
     }
   }
 }
