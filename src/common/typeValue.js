d282ce2163bb86cb5ce9d4d35abca53f572460d8
xiaobxia <673806687@qq.com>
2020-02-02 12:04:50 +0800

    xg

--- src/common/typeValue.js
+++ src/common/typeValue.js
@@ -1,4 +1,13 @@
 // 基于常量的选择框数据，列表数据格式化
+function findLabel(list, value) {
+  for (let i = 0; i < list.length; i++) {
+    const item = list[i]
+    if (item.value === value) {
+      return item.label
+    }
+  }
+  return ''
+}
 const USER_STATUS_LIST = [
   { value: '0', label: '禁用' },
   { value: '1', label: '启用' }
@@ -6,12 +15,6 @@ const USER_STATUS_LIST = [
 export default {
   USER_STATUS_LIST,
   formatUserStatus(value) {
-    for (let i = 0; i < USER_STATUS_LIST.length; i++) {
-      const item = USER_STATUS_LIST[i]
-      if (item.value === value) {
-        return item.label
-      }
-    }
-    return ''
+    return findLabel(USER_STATUS_LIST, value)
   }
 }
