41f506d59afd2b47b0935344ea7290e27f3700ff
xiaobxia <673806687@qq.com>
2020-03-04 16:04:15 +0800

    xg

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // import 'babel-polyfill'
 import Vue from 'vue'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
-import '../static/web-fonts-with-css/css/fontawesome-all.css'
+import '../static/web-fonts-with-css/css/font-awesome.css'
 import 'element-ui/lib/theme-chalk/index.css'
 import Element from 'element-ui'
 
