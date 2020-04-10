219122164544b9e658707a360b56d027c4d08ab0
xiaobxia <673806687@qq.com>
2020-04-10 09:34:14 +0800

    xg

--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="{fullscreen:fullscreen}" class="tinymce-container" :style="{width:containerWidth}">
+  <div :class="{fullscreen:fullscreen}" :style="{width:containerWidth}" class="tinymce-container">
     <textarea :id="tinymceId" class="tinymce-textarea" />
     <div class="editor-custom-btn-container">
       <editorImage color="#1890ff" class="editor-upload-btn" @successCBK="imageSuccessCBK" />
