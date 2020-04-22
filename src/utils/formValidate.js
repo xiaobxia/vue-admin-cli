b2bcce65bda57fb27220e50205822a0aa7a9b525
xiaobxia <673806687@qq.com>
2020-04-22 14:25:06 +0800

    xg

---
+++ src/utils/formValidate.js
@@ -0,0 +1,34 @@
+export default {
+  validatePhone(rule, value, callback) {
+    const re = /^1(3|4|5|6|7|8|9)\d{9}$/
+    if (!re.test(value)) {
+      callback(new Error('格式不正确!'))
+    } else {
+      callback()
+    }
+  },
+  validateTelephone(rule, value, callback) {
+    const re = /^(\(\d{3,4}\)|\d{3,4}-|\s)?\d{7,14}$/
+    if (!re.test(value)) {
+      callback(new Error('格式不正确!'))
+    } else {
+      callback()
+    }
+  },
+  validateIdCard(rule, value, callback) {
+    const re = /(^\d{15}$)|(^\d{17}([0-9]|X)$)/
+    if (!re.test(value)) {
+      callback(new Error('格式不正确!'))
+    } else {
+      callback()
+    }
+  },
+  validateEmail(rule, value, callback) {
+    const re = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/
+    if (!re.test(value)) {
+      callback(new Error('格式不正确!'))
+    } else {
+      callback()
+    }
+  }
+}
