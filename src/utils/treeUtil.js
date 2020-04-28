e5370f2574538d7e304175beafc6ddef45f113c8
xiaobxia <673806687@qq.com>
2020-04-28 10:35:12 +0800

    xg

--- src/utils/treeUtil.js
+++ src/utils/treeUtil.js
@@ -10,6 +10,42 @@ function getChildrenKeys(children, key, keys) {
   return keys
 }
 
+// 判断是否有一项在里面
+function ifChildrenIn(children, keys) {
+  for (let i = 0; i < children.length; i++) {
+    const item = children[i]
+    if (keys.indexOf(item.phid) !== -1) {
+      return true
+    } else {
+      if (item.childList && item.childList.length > 0) {
+        if (ifChildrenIn(item.childList, keys)) {
+          return true
+        }
+      }
+    }
+  }
+  return false
+}
+
+// 判断是否都在里面
+function ifChildrenAllIn(children, keys) {
+  for (let i = 0; i < children.length; i++) {
+    const item = children[i]
+    if (keys.indexOf(item.phid) === -1) {
+      return false
+    } else {
+      if (item.childList && item.childList.length > 0) {
+        if (!ifChildrenAllIn(item.childList, keys)) {
+          return false
+        }
+      }
+    }
+  }
+  return true
+}
+
 export default {
-  getChildrenKeys
+  getChildrenKeys,
+  ifChildrenIn,
+  ifChildrenAllIn
 }
