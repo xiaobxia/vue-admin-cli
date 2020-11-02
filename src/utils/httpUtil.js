b545d59adb0bfceb0763bc458edd2c85d2f6bb15
xiaobxia <673806687@qq.com>
2020-11-02 13:23:16 +0800

    xg

--- src/utils/httpUtil.js
+++ src/utils/httpUtil.js
@@ -58,6 +58,48 @@ function makeUrl(url) {
   }
 }
 
+// 去除参数值前后的空格
+function trim(str) {
+  return str.replace(/(^\s*)|(\s*$)/g, '')
+}
+// 去除参数前后空格
+function trimParams(target) {
+  // 定义一个变量
+  let result
+  // 如果当前需要深拷贝的是一个对象的话
+  if (typeof target === 'object') {
+    // 如果是一个数组的话
+    if (Array.isArray(target)) {
+      result = [] // 将result赋值为一个数组，并且执行遍历
+      for (const i in target) {
+        // 递归克隆数组中的每一项
+        result.push(trimParams(target[i]))
+      }
+      // 判断如果当前的值是null的话；直接赋值为null
+    } else if (target === null) {
+      result = null
+      // 判断如果当前的值是一个RegExp对象的话，直接赋值
+    } else if (target.constructor === RegExp) {
+      result = target
+    } else {
+      // 否则是普通对象，直接for in循环，递归赋值对象的所有值
+      result = {}
+      for (const i in target) {
+        result[i] = trimParams(target[i])
+      }
+    }
+    // 如果不是对象的话，就是基本数据类型，那么直接赋值
+  } else {
+    if (typeof target === 'string') {
+      result = trim(target)
+    } else {
+      result = target
+    }
+  }
+  // 返回最终结果
+  return result
+}
+
 const Http = {
   get(url, query, options) {
     let queryString = ''
